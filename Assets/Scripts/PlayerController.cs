703ed179c2aea1517e62536414d6f35a86b49411
Kaique Sacchi <sacchikaique@gmail.com>
2020-11-24 20:20:39 -0300

    Added Player model and controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed, tilt;
+    public float xMin, xMax;
+
+    Rigidbody rigidBody;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Vertical");
+        rigidBody.velocity = new Vector3(-speed * moveHorizontal, 0.0f, 0.0f);
+        rigidBody.position = new Vector3(
+            Mathf.Clamp(rigidBody.position.x, xMin, xMax),
+            0.0f,
+            0.0f
+        );
+        rigidBody.rotation = Quaternion.Euler(0.0f, 0.0f, rigidBody.velocity.x * -tilt);
+    }
+}
